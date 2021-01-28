d45afa6a557ce4d2f2f5c311544adb9d8cd9a4d3
eric <jiangxiaoliang@rhcpa.com>
2021-01-28 14:30:01 +0800

    修改

--- commons/__init__.py
+++ commons/__init__.py
@@ -4,7 +4,10 @@
 @Time    : 2020-11-30 17:31
 @IDE     : PyCharm
 '''
-
+name = 'Eric-Tools'
+__version__ = "1.1.1"
+__author__ = 'Eric'
+__doc__ = "工具类"
 
 from . import exception_class
 from . import resize_image
