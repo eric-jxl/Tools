0ff3dd58be8a3ee58682563602c6890094edc203
eric <799050481@qq.com>
2021-12-22 11:41:43 +0800

    modiy commit mark
    Signed-off-by: eric <799050481@qq.com>

--- common/__init__.py
+++ common/__init__.py
@@ -4,25 +4,25 @@
 @Time    : 2020-11-30 17:31
 @IDE     : PyCharm
 '''
+import decorator
+import Abstract
+import convert_json
+import jwt_encrypt
+import send_email
+import readconfig
+import sftp
+import pgsql
+import encryption_classmethod
+import ip
+import logger
+import resize_image
+import exception_class
+
 name = 'Eric-Tools'
 __title__ = 'tools'
 __description__ = 'Python HTTP for Humans.'
-__version__ = "1.2.1"
+__version__ = "1.2.1.1"
 __author__ = 'Eric'
 __doc__ = ["Python Daily Development Tools"]
 __url__ = "https://github.com/Eric-jxl/Tools"
 __license__ = "Apache"
-
-import exception_class
-import resize_image
-import logger
-import ip
-import encryption_classmethod
-import pgsql
-import sftp
-import readconfig
-import send_email
-import jwt_encrypt
-import convert_json
-import Abstract
-import decorator
